f6ba656dfea444410eb9ecdc602a1cb927633c01
Maurice Zeitouni <morriszeitouni@Maurices-MBP.lan>
2017-10-21 19:17:05 -0700

    profile pic

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import '../Details/Details.css';
+import './About.css';
 import Scroll from 'react-scroll';
 var React=require('react');
 var Element    = Scroll.Element;
@@ -12,7 +13,7 @@ export const About = () =>  {
 
 			<div className="list">
 
-					<div className="listElement"><img src="img/profile_pic.png"/>
+					<div className="listElement"><img src="img/morris.png"/>
 					<h4>Morris Zeitouni</h4>
 					<h5>Founder</h5>
 					<p>As a loyal season ticket owner of the New York Mets, Morris faced the difficulties of managing his own season ticket, which led to the creation of Kartees. Prior to founding Kartees, Morris worked as a developer and consultant at IBM Watson, where he helped businesses design and deploy Artifically Intelligent solutions. Morris holds a B.S. in Industrial Engineering & Management Science from Northwestern University. </p>
@@ -24,7 +25,7 @@ export const About = () =>  {
 					<p>Joseph is aligned to Accenture's Oracle Analytics practice, where he serves clients in multiple functional and technical analytics delivery capacities surrounding advanced business intelligence enablement. Previouly at the Miami Heat, Joseph oversaw the development of multiple machine learning tools built around proprietary statistical data. Joseph holds a B.S. in Biomedical Engineering from the University of Miami. </p>
 					</div>
 
-					<div className="listElement"><img src="img/eddie.jpg"/>
+					<div className="listElement"><img src="img/eddie.png"/>
 					<h4>James Rosen</h4>
 					<h5>Lead Designer</h5>
 					<p>As a former marketing analyst at Apple, James found a passion for creating superior user experiences and delivering solutions for today's practical problems. James holds a B.A. in Marketing from the Wharton School of Business, and an M.S. in Product Design from the University of Pennsylvania.</p>
